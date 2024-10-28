ce7dda041b4a5a07b51104f53e0d808b4b03b290
nipunagg2604 <nipun23349@iiitd.ac.in>
2024-10-28 22:15:03 +0530

    making dummy_main

--- dummy_main.h
+++ dummy_main.h
@@ -1,7 +1,19 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+
 int dummy_main(int argc, char **argv);
 
 int main(int argc, char **argv) {
+    sigset_t sigset;
+    sigemptyset(&sigset);
+    sigaddset(&sigset,SIGCONT);
 
+    sigprocmask(SIG_BLOCK, &sigset, NULL);
+
+    int sig;
+
+    sigwait(&sigset, &sig);
     int ret = dummy_main(argc, argv);
 
     
--- fib.c
+++ fib.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "dummy_main.h"
 
 int fib(int n) 
 {
@@ -6,9 +7,9 @@ int fib(int n)
 	return fib(n-1) + fib(n-2);
 }
 
-int main()
-{
-	printf("%d\n", fib(43));
+int main(int argc, char **argv)
+{	printf("%s","hii");
+	printf("%d\n", fib(40));
 
 	return 0;
 }
